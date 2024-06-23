624d8a5709eb3116730a3e469d64a2887a618891
Vaibhav <rawatvaibhav42@gmail.com>
2024-06-23 18:41:47 +0530

    first commit

---
+++ project/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import ReactSerializer
+from rest_framework import status
+
+class ReactView(APIView):
+    def get(self,request): 
+        output=[{"employee":output.employee,
+                 "department":output.department}
+                 for output in React.objects.all()]
+        return Response(output)
+    
+    def post(self,request):
+        serializer=ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
